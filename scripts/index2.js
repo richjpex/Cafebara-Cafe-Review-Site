f3f3751bcfc0c43cdbdbc07a71953f851387a790
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-06-21 19:39:17 +0800

    sidebar in progress

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,6 +1,24 @@
 document.addEventListener("DOMContentLoaded",function() {
-
+    console.log("connected");
     document.getElementById("profilepicture").addEventListener("click",function() {
-        document.querySelector(".sidebar").style.display = "block";
+        openclosesidebar();
     });
+
+    document.querySelector(".btn-close").addEventListener("click",function() {
+        document.querySelector(".sidebar").style.display = "none";
+    });
+
+    function openclosesidebar(){
+        const sidebar = document.querySelector(".sidebar");
+        switch(sidebar.style.display){
+            case "none": 
+                sidebar.style.display = "block";
+                break;
+            case "block":
+                sidebar.style.display = "none";
+                break;
+            default:
+                sidebar.style.display = "none";
+        }
+    }
 });
