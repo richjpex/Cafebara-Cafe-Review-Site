d871a9d282c7e95d6b9861af31d049f306262c8f
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-08-07 04:29:37 +0800

    Feature/backendfix (#85)
    * fixed showing capybaras in search
    
    * show rating number on profile page
    
    * remove pagination
    
    * sorts the reviews in cafePage by most useful

--- controllers/controller.js
+++ controllers/controller.js
@@ -204,6 +204,11 @@ const controller = {
                 rating: cafe.rating
             };
 
+            //sort reviewList by upvotes
+            reviewList.sort(function(a, b){
+                return b.up/b.down - a.up/a.down;
+            });
+
             res.render("viewCafe", {
                 layout: 'cafeTemplate',
                 cafePage: cafeView,
@@ -456,6 +461,7 @@ const controller = {
                 open_details: cafeList[i].weekdays_avail,
                 cafeImg: cafeList[i].image,
                 price: cafeList[i].price,
+                rating: cafeList[i].rating
             });
         }
 
